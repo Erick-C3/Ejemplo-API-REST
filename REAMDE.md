0840dc6341f2ad3b5d3c52ccd508c01a8315f1fc
erick <>
2023-10-31 12:09:48 -0300

    configuracion para el deloy terminada

--- REAMDE.md
+++ REAMDE.md
@@ -5,3 +5,6 @@
 1. inicializar versionado con git
 1. crear el script en package.json ` "dev": "nodemon index.js" `
     - verificar si funciona con `npm run dev`
+
+
+1. `http://localhost:4000/api?param=2&param2=asd&param3=false`
--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(express.json());
+app.use(cors());
 
 app.get("", (req, res)=>{
     res.json({atributo: "<h1> Hola! </h1>"});
 });
 
 app.get("/api", (req, res)=>{
-    res.send("<h1> Hola 2! </h1>");
+    console.log(req.query);
+    res.send(`<h1> Hola 2! ${req.query.param3}</h1>`);
 });
 
 app.get("/api/:id", (req, res)=>{
@@ -15,10 +20,23 @@ app.get("/api/:id", (req, res)=>{
     res.send(`<h1> Hola 3! ${req.params.id}</h1>`);
 });
 
+app.post("/api", (req, res)=>{
+    console.log(req.body);
+    res.status(201).send(`<h1> POST! ${req.body[0]} </h1>`);
+});
 
 
-app.listen(4000, ()=>{
-    console.log("Servidor activo");
+app.put("/api", (req, res)=>{
+    res.json({atributo: "<h1> PUT </h1>"});
+});
+
+app.delete("/api", (req, res)=>{
+    res.json({atributo: "<h1> DELETE </h1>"});
+});
+
+
+app.listen( process.env.PORT || 4000, ()=>{
+    console.log("Servidor activo", process.env.PORT || 4000);
 })
 
 app.use((req, res)=>{
