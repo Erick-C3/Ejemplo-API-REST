0bc3a6215295ca2d50e12311365c6c3395fd1af4
Erick <erick.nahuelpaiva.001@gmail.com>
2023-11-01 20:42:29 -0300

    agregada query para crear tabla

--- conexionDB.js
+++ conexionDB.js
@@ -1,6 +1,16 @@
 import { createPool } from "mysql2/promise";
 import configuracion from "./configuracion.js";
 
+const CREAR_TABLA = `
+    CREATE TABLE IF NOT EXISTS producto (
+        id INT(11) NOT NULL AUTO_INCREMENT,
+        nombre VARCHAR(45) DEFAULT NULL,
+        precio INT(10) DEFAULT NULL,
+        imagen VARCHAR(400) DEFAULT NULL,
+        PRIMARY KEY(id)
+    );
+`;
+
 const pool = createPool({
     host: configuracion.HOST,
     port: configuracion.PORT,
@@ -8,6 +18,8 @@ const pool = createPool({
     password: configuracion.PASSWORD,
     database: configuracion.NAME
 });
+
+await pool.query(CREAR_TABLA);
 
 console.log("CONEXION A BASE DE DATOS INICIADA");
 
