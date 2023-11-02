3a9247c35b25283bcdfa353a86a18d6d98c40b15
erick <>
2023-11-02 12:51:09 -0300

    nombre de password variable entorno modificada

--- configuracion.js
+++ configuracion.js
@@ -4,7 +4,7 @@ config();
 
 const PORT = process.env.DB_PORT;
 const USER = process.env.DB_USER;
-const PASSWORD = process.env.PASSWORD;
+const PASSWORD = process.env.DB_PASSWORD;
 const NAME = process.env.DB_NAME;
 const HOST = process.env.DB_HOST;
 
