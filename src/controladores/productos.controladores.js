51435313da12cd6320736b81e1c8faf5f15c7432
Erick <erick.nahuelpaiva.001@gmail.com>
2023-11-07 20:19:06 -0300

    mejorada ruta de post

--- src/controladores/productos.controladores.js
+++ src/controladores/productos.controladores.js
@@ -30,15 +30,28 @@ async function obtenerProductos (req, res){
     res.send(`<h1> Hola 2! ${req.query.param3}</h1>`);
 }; */
 
+
+/**
+ * Devuelve el producto solicitado de la base de dato si existe
+ * @param {Object} req de la consulta
+ * @param {Object} res de la consulta
+ */
 async function obtenerProducto (req, res){
     const ID = req.params.id;
-    const [resultado] = await pool.query("SELECT * FROM producto WHERE id = ?", ID);
-    if (!resultado.length) {
-        res.status(404).json({
-            info: "No se encontro el producto con id: "+ID
-        });
-    }else{
-        res.json(resultado);
+    try {
+        const [resultado] = await pool.query("SELECT * FROM producto WHERE id = ?", ID);
+        if (!resultado.length) {
+            res.status(404).json({
+                info: "No se encontro el producto con id: "+ID
+            });
+        }else{
+            res.json(resultado);
+        }
+    } catch (error) {
+        res.status(500).json({
+            informe: "Algo salio mal",
+            error: error
+        })
     }
 };
 
@@ -46,7 +59,28 @@ async function obtenerProducto (req, res){
 
 
 async function crearProducto (req, res){
-    res.status(201).json(req.body);
+    const {nombre, precio, imagen} = req.body;
+
+    try {
+        const [info] = await pool.query(`INSERT INTO producto(nombre, precio, imagen) VALUES(?, ?, ?);`, [nombre, precio, imagen]);
+        const [resultado] = await pool.query("SELECT * FROM producto WHERE id = ?;", [info.insertId]);
+        console.log(info.affectedRows);
+        if (info.affectedRows !== 1 || !resultado.length) {
+            res.status(404).json({
+                mensaje: "Error al agregar producto"
+            });
+        }else{
+            res.status(201).json({
+                idNuevo: info.insertId,
+                producto: resultado
+            });
+        }
+    } catch (error) {
+        res.status(500).json({
+            informe: "Algo salio mal al agregar",
+            error: error
+        })
+    }
 };
 
 
