9e787c96034b76e3177c95111a42157f2d088b3d
Erick <erick.nahuelpaiva.001@gmail.com>
2023-11-09 23:12:51 -0300

    control de acceso implementado

--- src/app.js
+++ src/app.js
@@ -4,8 +4,21 @@ import productosRutas from "./rutas/productos.rutas.js";
 
 const app = express();
 
+const urlPermitidas = ["http://127.0.0.1:5500"];
+
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: (origin, callback)=>{
+        console.log(origin);
+        if (urlPermitidas.includes(origin) || !origin) {
+            callback(null, true);
+        }else{
+            const error = new Error("Error: no permitido por CORS");
+            error.status = 401;
+            callback(error, false);
+        }
+    }
+}));
 
 app.use("/productos/",productosRutas)
 app.use((req, res)=>{
